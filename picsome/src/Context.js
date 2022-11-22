fa102dc08679316b221912889581e3f00ba9906b
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-11-22 09:44:40 -0500

    initial comit

---
+++ picsome/src/Context.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react"
+
+const Context = React.createContext()
+
+function ContextProvider({children}) {
+    const [allPhotos, setAllPhotos] = useState([])
+    
+    return (
+        <Context.Provider value={{allPhotos}}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export {ContextProvider, Context}
