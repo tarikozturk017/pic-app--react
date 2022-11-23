e5e931774e43d70b73ebedaf556abad03a773318
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-11-22 22:06:54 -0500

    toggle heart

--- picsome/src/Context.js
+++ picsome/src/Context.js
@@ -1,12 +1,32 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
     const [allPhotos, setAllPhotos] = useState([])
     
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+    
+    function toggleFavorite(id) {
+        const updatedArr = allPhotos.map(photo => {
+            if(photo.id === id) {
+                console.log(id)
+                console.log(!photo.isFavorite)
+                return {...photo, isFavorite: !photo.isFavorite}
+            }
+            return photo
+        })
+        
+        setAllPhotos(updatedArr)
+    }
+    
     return (
-        <Context.Provider value={{allPhotos}}>
+        <Context.Provider value={{allPhotos, toggleFavorite}}>
             {children}
         </Context.Provider>
     )
---
+++ picsome/src/components/Image.js
@@ -0,0 +1,31 @@
+import React, {useState, useContext} from "react"
+import {Context} from "../Context"
+
+function Image({className, img}) {
+    const [hovered, setHovered] = useState(false)
+    const {toggleFavorite} = useContext(Context)
+    
+    function heartIcon() {
+        if(img.isFavorite) {
+            return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
+        } else if(hovered) {
+            return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+        }
+    }
+
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+    
+    return (
+        <div 
+            className={`${className} image-container`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+            <img src={img.url} className="image-grid"/>
+            {heartIcon()}
+            {cartIcon}
+        </div>
+    )
+}
+
+export default Image
